59508c3b35a2bf82e7c2390bf86862e2d902ece3
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2020-05-08 19:54:30 +1100

    feat: Store + Wordpress API call + Homepage

--- store/index.js
+++ store/index.js
@@ -22,7 +22,7 @@ export const actions = {
       posts = posts
         .filter(el => el.status === "publish")
         .map(({ id, slug, title, excerpt, date, tags, content }) => ({
-          id
+          id,
           slug,
           title,
           excerpt,
